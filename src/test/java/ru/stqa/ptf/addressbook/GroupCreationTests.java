3801d52590012aacb47854ca52e4d711e81bf8ef
Svyatoslav Vozgrin <Svjatoslav.Vozgrin@gmail.com>
2018-11-21 12:23:11 +0200

    Fixtura

--- src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
+++ src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
@@ -1,29 +1,28 @@
 package ru.stqa.ptf.addressbook;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.fail;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
   private WebDriver driver;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     driver = new ChromeDriver();
-
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
+    driver.get("http://localhost/addressbook/group.php");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
@@ -36,18 +35,18 @@ public class GroupCreationTests {
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("TestOne");
+    driver.findElement(By.name("group_name")).sendKeys("Y7");
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("TestTwo");
+    driver.findElement(By.name("group_header")).sendKeys("Y7");
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("TestThree");
+    driver.findElement(By.name("group_footer")).sendKeys("Y7");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
